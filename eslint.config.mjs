7672562afc0cdf85693c08a13b210ec91dddcfe4
Claire Sylvester <clairesylvester@clairecodes.dev>
2025-09-15 20:29:27 -0700

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,31 +11,31 @@ const compat = new FlatCompat({
 
 // This creates a config array with all of the plugins and settings for Next.js core-web-vitals
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-  ...compat.extends("prettier"),
+  ...compat.extends('next/core-web-vitals'),
+  ...compat.extends('prettier'),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      ".vercel/**",
-      "coverage/**",
-      "*.tsbuildinfo",
-      "next-env.d.ts",
-    ]
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      '.vercel/**',
+      'coverage/**',
+      '*.tsbuildinfo',
+      'next-env.d.ts',
+    ],
   },
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     rules: {
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "arrow-body-style": ["warn", "as-needed"],
-      "no-var": "error",
-      "prefer-const": "warn",
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-    }
-  }
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'arrow-body-style': ['warn', 'as-needed'],
+      'no-var': 'error',
+      'prefer-const': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error', 'log'] }],
+    },
+  },
 ];
 
 export default eslintConfig;
